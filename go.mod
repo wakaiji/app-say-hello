5c8b709efc84119e0915335e64dacf70eaa2b48b
wira ardi <wiraardi79@gmail.com>
2022-04-18 11:39:42 +0700

    add new project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wiraline/app-say-hello
+
+go 1.17
+
+require github.com/wiraline/go-say-hello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gosayhello "github.com/wiraline/go-say-hello/v2"
+)
+
+func main() {
+	fmt.Println(gosayhello.SayHello("Wira"))
+}
